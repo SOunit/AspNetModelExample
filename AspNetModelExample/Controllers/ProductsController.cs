885ebfd92101bfbbf08030630978afa31885a594
SOunit <workbookwork7@gmail.com>
2022-10-22 15:08:00 -0700

    add sample

---
+++ AspNetModelExample/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using AspNetModelExample.Models;
+
+namespace AspNetModelExample.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product() { ProductId = 101, ProductName = "AC", Rate = 45000 },
+                new Product() { ProductId = 102, ProductName = "Mobile", Rate = 38000 },
+                new Product() { ProductId = 103, ProductName = "Bike", Rate = 94000 },
+            };
+
+            return View(products);
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
---
+++ AspNetModelExample/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace AspNetModelExample.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public double Rate { get; set; }
+    }
+}
